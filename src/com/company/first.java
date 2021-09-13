538f36b938cec778eafc8145a8d5e69c40f9a05f
jiao-long <2622491932@qq.com>
2021-09-13 09:36:12 +0800

    hot-fix第3次提交

--- src/com/company/first.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class first {
-    public static void main(String[] args) {
-        System.out.println("这是first文件");
-        //本次修改：删除了master分支的一行代码，同时新增了一行hot-fix分支的代码
-        System.out.println("这是first文件——hot-fix分支特意新增");
-    }
-}
--- src/com/company/second.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class second {
-    public void test(){
-        System.out.println("这是一个测试方法");
-        System.out.println("在原有的master分支上新增一行内容");
-    }
-}
