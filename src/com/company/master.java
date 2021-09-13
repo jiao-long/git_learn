f19d4f287fc64de93743a7fe16626ad4d2a96eb7
jiao-long <2622491932@qq.com>
2021-09-13 11:26:14 +0800

    master 2+3

--- src/com/company/master.java
+++ src/com/company/master.java
@@ -4,5 +4,7 @@ package com.company;
 public class master {
     public static void main(String[] args) {
         String a="master 1";
+        String b="master 2";
+        String c="master 3";
     }
 }
