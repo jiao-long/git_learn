139d2913a145ccea6c3edc6142d656da47623c93
liuzhangqi <zhangqi.liu@pactera.com>
2021-11-13 15:16:07 +0800

    test-A类第二次修改

--- src/com/company/A.java
+++ src/com/company/A.java
@@ -4,4 +4,11 @@ public class A {
     public void getNmae(){
         System.out.println("tt分支新建一个类");
     }
+    public void newB(){
+        System.out.println("新增B方法");
+    }
+    public int sum(int a,int b)
+    {
+        return a+b;
+    }
 }
