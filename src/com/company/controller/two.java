702c7da2778e2bda21da0fe4a6e072ad2f59a46c
liuzhangqi <zhangqi.liu@pactera.com>
2021-11-13 15:03:37 +0800

    master删除了一个方法

--- src/com/company/controller/two.java
+++ src/com/company/controller/two.java
@@ -1,10 +1,10 @@
 package com.company.controller;
 
 public class two {
+    private String name="name";
     public void test1(){
         System.out.println("这是一个补丁文件测试");
+        System.out.println("master删除了新增的方法");
     }
-    public void newMethod(){
-        System.out.println("新增一个方法");
-    }
+
 }
